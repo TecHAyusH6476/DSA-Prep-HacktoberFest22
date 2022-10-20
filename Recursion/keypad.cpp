c4732f9a1715c6e7ca1a613192447c7e45a69a33
Bajaj2O <sajal2bajaj@gmail.com>
2022-10-20 20:08:09 +0530

    added recursive problem

---
+++ Recursion/keypad.cpp
@@ -0,0 +1,42 @@
+// Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+// A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+  /*    1 -> none     2 -> abc         3->def  
+        4->ghi        5-> jkl         6-> mno
+        7 ->pqrs      8->tuv           9->wxyz
+*/ 
+
+
+class Solution {
+private:
+    void solve(string digit, string output, int index, vector<string>& ans, string mapping[] ) {
+        
+        //base case
+        if(index >= digit.length()) {
+            ans.push_back(output);
+            return;
+        }
+        
+        int number = digit[index] - '0';
+        string value = mapping[number];
+        
+        for(int i=0; i<value.length(); i++) {
+            output.push_back(value[i]);
+            solve(digit, output, index+1, ans, mapping);
+            output.pop_back();
+        }
+        
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        if(digits.length()==0)
+            return ans;
+        string output;
+        int index = 0;
+        string mapping[10] = {"", "", "abc", "def", "ghi", "jkl","mno","pqrs","tuv","wxyz"};
+        solve(digits, output, index, ans, mapping);
+        return ans;
+    }
+};
